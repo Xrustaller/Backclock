821710251e569e1d5e9882eecd9877129368ac63
Xrustaller <stalker0077@gmail.com>
2022-02-04 11:33:22 +0300

    v1.1

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ window.onload = function() {
   setInterval(function() {
     var hours = new Date().getHours();
     var minutes = new Date().getMinutes();
-    var seconds = new Date().getSeconds();
+    //var seconds = new Date().getSeconds();
     document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
     document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
-    document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
+    //document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
   }, 1000);
 }
